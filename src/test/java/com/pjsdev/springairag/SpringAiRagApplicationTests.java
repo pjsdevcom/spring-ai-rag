032ab3c885577c878cbe28013e7f6dc87047d756
pjsdevcom <mail@pjsdev.com>
2025-06-02 21:44:32 +0100

    first commit

---
+++ src/test/java/com/pjsdev/springairag/SpringAiRagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjsdev.springairag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiRagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
