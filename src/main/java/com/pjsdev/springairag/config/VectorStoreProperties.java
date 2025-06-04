dcec5f31f2df63936392412dfcb9a74ce8e3bd0e
pjsdevcom <mail@pjsdev.com>
2025-06-04 20:51:02 +0100

    Configure vector store data load

--- src/main/java/com/pjsdev/springairag/config/VectorStoreProperties.java
+++ src/main/java/com/pjsdev/springairag/config/VectorStoreProperties.java
@@ -4,6 +4,9 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+import java.util.List;
 
 @Setter
 @Getter
@@ -12,4 +15,5 @@ import org.springframework.context.annotation.Configuration;
 public class VectorStoreProperties {
 
     private String vectorStorePath;
+    private List<Resource> documentsToLoad;
 }
